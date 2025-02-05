31b2e07fb103842c6c85dcaa30bd9dd7223b861d
vxzquz <jvazber1512@g.educaand.es>
2025-02-05 13:22:18 +0100

    Update gabriel.js

--- gabriel.js
+++ gabriel.js
@@ -1,7 +1,8 @@
 
 
-var x = prompt ("Escribe un numero del 1 al 100 ");
+var numero = prompt ("Escribe un numero del 1 al 100 ");
+numero = Number(numero);
 
-  if (x<0 || x>100) {
+  while (numero<0 || numero>100) {
     document.write ("Por tanto, si tu ojo derecho te hace pecar, sácatelo y tíralo. Más te vale perder una sola parte de tu cuerpo, y no que todo él sea arrojado al infierno.");
   }
