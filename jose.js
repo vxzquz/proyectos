c28a3638c5642d7b14eb360381658e8d60a21f2b
vxzquz <jvazber1512@g.educaand.es>
2025-02-10 12:11:35 +0100

    Create jose.js

---
+++ jose.js
@@ -0,0 +1,10 @@
+function jose(min, max) {
+var numero;
+do {
+
+numero = prompt ("escribe un número entre " + min +" y " + max + ":");
+numero = Number(numero);
+
+  while (numero<min || numero>max) {
+    return numero;
+  }
