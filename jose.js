433793b9166b6109cb3a5b2a16c830b0d46d6ff1
vxzquz <jvazber1512@g.educaand.es>
2025-02-12 13:24:09 +0100

    Update jose.js

--- jose.js
+++ jose.js
@@ -1,10 +1,11 @@
-function jose(min, max) {
-var numero;
-do {
-
-numero = prompt ("escribe un número entre " + min +" y " + max + ":");
-numero = Number(numero);
-
-  while (numero<min || numero>max) {
-    return numero;
-  }
+function jose(min, max) 
+{
+  var numero;
+  do 
+    {
+    numero = prompt ("escribe un número entre " + min +" y " + max + ":");
+    numero = Number(numero);
+    }  
+  while (numero<min || numero>max) 
+return numero;
+}
