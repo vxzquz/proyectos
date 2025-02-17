bf00c5a212edb2f03f2f89af2b6db515442a4031
vxzquz <jvazber1512@g.educaand.es>
2025-02-17 12:11:42 +0100

    Create primoono.js

---
+++ primoono.js
@@ -0,0 +1,18 @@
+function primoono (numero)
+  {
+    var Primosi;
+    var i;
+    
+    for (i=numero-1; i>=2; i=i-1 )
+      {
+        if ( numero % i == 0 )
+        {
+          Primosi = false;
+        }
+
+
+        
+       }
+    Primosi = true;
+    return Primosi;
+  }
