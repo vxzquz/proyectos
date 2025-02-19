f9da1956f292ce7b1f6fa19cacfe0edab66075c5
vxzquz <jvazber1512@g.educaand.es>
2025-02-19 13:19:25 +0100

    Create parono.js

---
+++ parono.js
@@ -0,0 +1,16 @@
+function parono (numero)
+  {
+    var parsi;
+    var i = 2;
+    
+      
+        if ( numero % i == 0 )
+        {
+          parsi = true;
+          return parsi;
+        }
+     
+       
+    parsi = false;
+    return parsi;
+  }
